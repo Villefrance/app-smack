f447d5f71900c0e56fdd11a53db73ed6ce8afbb9
Villefrance <emilkoch@gmail.com>
2017-07-31 23:17:16 +0200

    End of lesson 4.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Emil Villefrance on 31/07/2017.
+//  Copyright © 2017 Emil Villefrance. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
