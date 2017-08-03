c4700386ce959ceae96c3bc13ed8cfd4fa88e956
Villefrance <emilkoch@gmail.com>
2017-08-03 19:47:21 +0200

    End of lesson 12.

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Emil Villefrance on 03/08/2017.
+//  Copyright © 2017 Emil Villefrance. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+}
