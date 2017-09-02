4c6a0524cbc8cf9aecaee0e278cc63ef24a63fbf
Villefrance <emilkoch@gmail.com>
2017-09-02 13:14:24 +0200

    End of lesson 20.

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -12,7 +12,7 @@ class ChatVC: UIViewController {
 
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
-    
+    @IBOutlet weak var channelNameLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,15 +20,41 @@ class ChatVC: UIViewController {
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
         
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNEL_SELECTED, object: nil)
+        
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail(completion: { (success) in
                 NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
             })
         }
-        MessageService.instance.findAllChannels { (success) in
-            
-        }
+        
     }
 
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            // Get channels
+            onLoginGetMessages()
+        } else {
+            channelNameLbl.text = "Please Log In"
+        }
+    }
+    
+    @objc func channelSelected(_ notif: Notification) {
+        updateWithChannel()
+    }
+    
+    func updateWithChannel() {
+        let channelName = MessageService.instance.selectedChannel?.name ?? ""
+        channelNameLbl.text = "#\(channelName)"
+    }
+    
+    func onLoginGetMessages() {
+        MessageService.instance.findAllChannels { (success) in
+            if success {
+                // Do stuff with channels
+            }
+        }
+    }
 
 }
--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,6 +15,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var selectedChannel: Channel?
     
     func findAllChannels(completion: @escaping CompletionHandler) {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
@@ -27,8 +28,8 @@ class MessageService {
                 } catch let error {
                     debugPrint(error as Any)
                 }
+                NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                 
-                print(self.channels)
 //                if let json = JSON(data: data).array {
 //                    for item in json {
 //                        let name = item["name"].stringValue
@@ -38,13 +39,17 @@ class MessageService {
 //                        self.channels.append(channel)
 //                    }
 //
-//                    completion(true)
+                    completion(true)
 //                }
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func clearChannels() {
+        channels.removeAll()
     }
     
 }
